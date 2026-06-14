e26334512564b77a18264ffe0e0a4cc24c01ec9e
Seb <51987128+sebhkx@users.noreply.github.com>
2026-06-14 16:18:43 +0800

    Update last-week-date.sh
    now it's smarter but still anchored to this week's monday. allow last & last last and next & next next

--- last-week-date.sh
+++ last-week-date.sh
@@ -1,57 +1,77 @@
-## Raycast script for copying to clipboard last day's date into clipboard
-## if input yesterday's day, result will be the day from last week
-
-#!/bin/bash 
-
+#!/bin/bash
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Smart Last Week Date
-# @raycast.commandName last-week-date
-# @raycast.mode compact
-# @raycast.argument1 { "type": "text", "placeholder": "mon | tue | wed | thu | fri | sat | sun | range" }
+# @raycast.title Auto Day Date
+# @raycast.commandName day-auto-week-date
+# @raycast.mode silent
+# @raycast.packageName Date Utilities
 
-FORMAT="%Y/%m/%d"
+# @raycast.argument1 { "type": "text", "placeholder": "mon - sun | last / next | last last | next next .."}
+# Optional parameters:
+# @raycast.icon 📅
+
+FORMAT="+%Y/%m/%d"
+
 ARG=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-weekday_num() {
+
+copy() {
+  osascript -e "set the clipboard to \"$1\""
+}
+
+weekday_offset() {
   case "$1" in
-    mon) echo 1 ;;
-    tue) echo 2 ;;
-    wed) echo 3 ;;
-    thu) echo 4 ;;
-    fri) echo 5 ;;
-    sat) echo 6 ;;
-    sun) echo 7 ;;
+    mon) echo 0 ;;
+    tue) echo 1 ;;
+    wed) echo 2 ;;
+    thu) echo 3 ;;
+    fri) echo 4 ;;
+    sat) echo 5 ;;
+    sun) echo 6 ;;
+    *) echo "" ;;
   esac
 }
 
+# count modifiers
+LAST_COUNT=$(echo "$ARG" | grep -o "last" | wc -l | xargs)
+NEXT_COUNT=$(echo "$ARG" | grep -o "next" | wc -l | xargs)
+
+CLEAN=$(echo "$ARG" | sed -E 's/(last|next)//g' | xargs)
+
+# net week shift
+WEEK_SHIFT=$(( NEXT_COUNT - LAST_COUNT ))
+
+# anchor: this week's Monday
+TODAY_EPOCH=$(date +%s)
+TODAY_WEEKDAY=$(date +%u)
+MONDAY_EPOCH=$(( TODAY_EPOCH - ((TODAY_WEEKDAY - 1) * 86400) ))
+
 get_date() {
-  TARGET="$1"
-  TODAY=$(date +%u)
-  TARGET_NUM=$(weekday_num "$TARGET")
-  DIFF=$((TODAY - TARGET_NUM))
-  if [ "$DIFF" -gt 1 ]; then
-    date -v-"$TARGET" +"$FORMAT"
-  else
-    date -v-"$TARGET" -v-1w +"$FORMAT"
-  fi
+  local day_offset="$1"
+  local week_shift="$2"
+
+  local total_days=$((week_shift * 7 + day_offset))
+  local target_epoch=$((MONDAY_EPOCH + total_days * 86400))
+
+  date -r "$target_epoch" "$FORMAT"
 }
 
-copy_to_clipboard() {
-  RESULT="$1"
-  osascript -e "set the clipboard to \"$RESULT\""
-}
-
-case "$ARG" in
-  mon|tue|wed|thu|fri|sat|sun)
-    RESULT=$(get_date "$ARG")
-    copy_to_clipboard "$RESULT"
-    ;;
+case "$CLEAN" in
   range)
-    START=$(get_date mon)
-    END=$(get_date sun)
-    RESULT="$START - $END"
-    copy_to_clipboard "$RESULT"
+    START_EPOCH=$((MONDAY_EPOCH + WEEK_SHIFT * 7 * 86400))
+    START=$(date -r "$START_EPOCH" "$FORMAT")
+    END=$(date -r $((START_EPOCH + 6 * 86400)) "$FORMAT")
+    copy "$START - $END"
     ;;
+
   *)
+    OFFSET=$(weekday_offset "$CLEAN")
+
+    if [ -z "$OFFSET" ]; then
+      exit 0
+    fi
+
+    RESULT=$(get_date "$OFFSET" "$WEEK_SHIFT")
+    copy "$RESULT"
+    ;;
 esac
